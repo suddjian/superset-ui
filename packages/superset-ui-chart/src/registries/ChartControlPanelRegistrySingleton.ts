7dc5712b268aa08226adf95b8f2f8a4972a608be
David Aaron Suddjian <aasuddjian@gmail.com>
2020-04-15 15:45:20 -0700

    feat: getInventory method on the chart registry

--- packages/superset-ui-chart/src/registries/ChartControlPanelRegistrySingleton.ts
+++ packages/superset-ui-chart/src/registries/ChartControlPanelRegistrySingleton.ts
@@ -1,9 +1,25 @@
+import memoize from 'lodash/memoize';
 import { Registry, makeSingleton } from '@superset-ui/core';
 import { ChartControlPanel } from '../models/ChartControlPanel';
 
 class ChartControlPanelRegistry extends Registry<ChartControlPanel, ChartControlPanel> {
   constructor() {
     super({ name: 'ChartControlPanel' });
+    this.getInventory = memoize(this.getInventory.bind(this));
+  }
+
+  getInventory(vizType: string) {
+    const controls = this.get(vizType);
+    if (!controls) return null;
+    const controlsMap: { [key: string]: any } = {};
+    controls.controlPanelSections.forEach((section: any) => {
+      section.controlSetRows.forEach((row: any) => {
+        row.forEach((control: any) => {
+          controlsMap[control.name] = control.config;
+        });
+      });
+    });
+    return controlsMap;
   }
 }
 
